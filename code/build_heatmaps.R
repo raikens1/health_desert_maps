c5cdd42ebc8c8a813559cdd708dd27d426a76a76
raikens1 <rockyaikens@gmail.com>
2019-04-22 15:53:18 -0700

    Starter comments on input and output format

---
+++ code/build_heatmaps.R
@@ -0,0 +1,18 @@
+# Build Heatmaps
+
+# This file should contain code that reads in a data file (csv format)
+# of travel times to hospital in an area of interest
+# and produce a heatmap of travel times to hospital over that area
+
+# The input file should have rows which each represent a distinct start location
+# The columns of the input file should be:
+# census_tract_id: `string`
+# start_longitude: `string?`
+# start_latitude: `string?`
+# t_1: `int` minutes to nearest hospital
+# t_2: `int` minutes to second nearest hospital
+# t_3: `int` minutes to third nearest hospital
+
+# This file may have comment lines written above the data
+# Comment lines all must begin with #
+# The column headings will be included. This line should not start with a header
