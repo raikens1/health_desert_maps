e111f1acfc68b3b973a4979b2e1fe6b6c755129d
raikens1 <rockyaikens@gmail.com>
2019-05-09 18:00:07 -0700

    basic census tract maps

--- code/build_heatmaps.R
+++ code/build_heatmaps.R
@@ -16,3 +16,42 @@
 # This file may have comment lines written above the data
 # Comment lines all must begin with #
 # The column headings will be included. This line should not start with a header
+
+library(sf) # dependency for tmap
+library(raster)
+library(spData)
+
+library(dplyr)
+library(tmap)    # for static and interactive maps
+library(leaflet) # for interactive maps
+library(mapview) # for interactive maps
+library(ggplot2) # tidyverse vis package
+library(shiny)   # for web applications
+library(stringr)
+library(tidycensus)
+
+census_api_key("6020dd87f4d614074553da9b317878cb026a7c88")
+
+CA_pop <- get_acs(geography = "tract", 
+                     variables = "B01003_001", 
+                     state = "CA",
+                     geometry = TRUE) 
+
+CA_pop
+
+pal <- colorQuantile(palette = "viridis", domain = CA_pop$estimate, n = 5)
+
+CA_pop %>%
+  st_transform(crs = "+init=epsg:4326") %>%
+  leaflet(width = "100%") %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>%
+  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
+              stroke = FALSE,
+              smoothFactor = 0,
+              fillOpacity = 0.7,
+              color = ~ pal(estimate)) %>%
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ estimate,
+            title = "Population percentiles",
+            opacity = 1)
