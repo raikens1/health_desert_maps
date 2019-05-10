17776770e45d66b5a1dd92f5e2fdc5fcc5ec3556
Ben Huynh <benhuynh3@gmail.com>
2019-05-09 19:02:20 -0700

    made distance matrix. too big to upload, but easily retrieved by running this code

--- code/hospitalDataClean.R
+++ code/hospitalDataClean.R
@@ -2,7 +2,7 @@ library(readr)
 library(dplyr)
 hosp17_util_data_final <- read_csv("~/Downloads/hosp17_util_data_final.csv")
 hospClean <- hosp17_util_data_final %>%
-  select(FAC_NAME,FAC_ADDRESS_ONE,LATITUDE,LONGITUDE,COUNTY,TYPE_LIC,CENS_TRACT,FAC_CITY,TYPE_SVC_PRINCIPAL)
+  select(FAC_NAME,FAC_ADDRESS_ONE,LATITUDE,LONGITUDE,COUNTY,TYPE_LIC,CENS_TRACT,FAC_CITY,TYPE_SVC_PRINCIPAL,OSHPD_ID)
 hospClean <- hospClean[-(1:3),]
 library(sf)
 shape <- read_sf(dsn = "./tl_2017_06_tract", layer = "tl_2017_06_tract")
@@ -11,18 +11,27 @@ shape <- read_sf(dsn = "./tl_2017_06_tract", layer = "tl_2017_06_tract")
 
 library(rdist)
 
-coordMat1 <- hospClean %>% select(FAC_NAME,LATITUDE,LONGITUDE)
+coordMat1 <- hospClean %>% select(LATITUDE,LONGITUDE)
 coordMat1$LATITUDE <- as.numeric(coordMat1$LATITUDE)
 coordMat1$LONGITUDE <- as.numeric(coordMat1$LONGITUDE)
-coordMat2 <- shape %>% as.data.frame() %>%  select(GEOID,INTPTLAT,INTPTLON) 
+coordMat2 <- shape %>% as.data.frame() %>%  select(INTPTLAT,INTPTLON) 
 coordMat2$INTPTLAT <- as.numeric(coordMat2$INTPTLAT)
 coordMat2$INTPTLON <- as.numeric(coordMat2$INTPTLON)
 distances <- cdist(coordMat1,coordMat2)
-rownames(distances) <- hospClean$FAC_NAME
+rownames(distances) <- hospClean$OSHPD_ID
 colnames(distances) <- shape$GEOID
 
 library(reshape)
 distanceDF <- melt(distances)[melt(upper.tri(distances))$value,]
-colnames(distanceDF) <- c("Hospital","CensusTractGEOID","Distance")
+colnames(distanceDF) <- c("OSHPDID","CensusTractGEOID","Distance")
 
-distanceDF$hospLat <- hospClean$LATITUDE[hospClean$FAC_NAME==distanceDF$Hospital]
+
+coordDF1 <- coordMat1
+coordDF2 <- coordMat2
+coordDF1$OSHPD_ID <- as.integer(hospClean$OSHPD_ID)
+coordDF2$GEOID <- as.numeric(shape$GEOID)
+
+distanceDFMerge <- left_join(distanceDF,coordDF1,by=c("OSHPDID"="OSHPD_ID"))
+colnames(distanceDFMerge)[4:5] <- c("HospLat","HospLon")
+distanceDFMerge <- left_join(distanceDFMerge,coordDF2,by=c("CensusTractGEOID"="GEOID"))
+colnames(distanceDFMerge)[6:7] <- c("TractLat","TractLon")
